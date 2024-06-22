c5bb7686baa40049b67b9df4e3b84272305950bb
blackroot <smallfish_root@126.com>
2024-06-22 13:03:12 +0800

    http接口相关注释

--- bot/constants/request.py
+++ bot/constants/request.py
@@ -1,3 +1,5 @@
+# pydantic常用于数据接口schema定义与检查的库
+# 在python中定义请求结构即是定义class类变量
 import pydantic
 from pydantic import BaseModel
 from typing import List
@@ -6,6 +8,7 @@ class OpenAIMessage(BaseModel):
     role: str = "user"
     content: str = "hello"
 
+# 通过class类把相关的对象变量组织成一个class类(作用类似struct)作为一个整体
 class OpenAIChatMsgIn(BaseModel):
     model: str = "gpt-3.5-turbo"
     messages: List[OpenAIMessage]
--- bot/constants/response.py
+++ bot/constants/response.py
@@ -1,7 +1,11 @@
+# pydantic常用于数据接口schema定义与检查的库
+# 在python中定义响应结构即是定义class类变量
 import pydantic
 from pydantic import BaseModel
 
 class BaseResponse(BaseModel):
+    # 定义并初始化
+    # code: int 说明定义字段将被赋值的对象类型
     code: int = pydantic.Field(200, description="HTTP status code")
     msg: str = pydantic.Field("success", description="HTTP status message")
     data: dict = pydantic.Field({}, description="HTTP response data")
